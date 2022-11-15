ea3728598f7024387d5471c319e9efc06c53f0cb
Matheus Carvalho <matheus12bh@gmail.com>
2022-11-15 12:14:05 -0800

    add-all-usecases

---
+++ src/app/useCases/products/listProducts.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+import { Product } from '../../models/Product';
+
+export async function listProducts(req: Request, res: Response) {
+  try {
+    const products = await Product.find();
+
+    return res.status(200).json(products);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal server error!' });
+  }
+}
