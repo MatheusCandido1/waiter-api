60053afaa680980b83e58d9ed6d21ba9ce69dcdf
Matheus Carvalho <matheus12bh@gmail.com>
2022-11-19 00:40:35 -0800

    add-socket

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
 import path from 'node:path';
+import http from 'node:http';
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { Server } from 'socket.io';
+
 import { router } from './router';
+
+
+const app = express();
+
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose.connect('mongodb://127.0.0.1:27017')
   .then(() => {
-    const app = express();
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+      next();
+    });
 
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
     app.use(express.json());
     app.use(router);
 
-    app.listen(3001, () => {
+    server.listen(3001, () => {
       console.log('Server started on http://localhost:3001');
     });
   })
